8863e75c8638649380f7c7a86bf6c2a63dd36264
unknown <mariannybatista2003@gmail.com>
2022-05-19 11:33:08 -0400

    Mostrar la data desde main.js

--- src/data.js
+++ src/data.js
@@ -1,9 +1,15 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return "example";
-};
+/*CREAR FUNCIONES SEGUN LOS FILTRADOS QUE SE NECESITA EN LA PAGINA PARA IMPORTARLOS EN MAIN.JS, manipular la data de por si */
 
-export const anotherExample = () => {
-  return "OMG";
-};
+//export const getData = (data) => {};
+
+/*export almacena esta funcion-> const filterByRole = trabaja con los objetos (rolChoice, allData) =>{
+  return allData.filter(campeon => campeon.tags.includes(rolChoice))
+};*/
+
+// ctrl+k y ctrl+c || ctrl+k y ctrl+u
+
+/* Array.isArray(championList) ? championList : Object.values(championList);*/
+
+/*console.log(Object.Values(data));*/
